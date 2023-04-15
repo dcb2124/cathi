9f1a0e7a66c1999bbc98a1fc1cbbed24edd2d9c8
David Billingsley <dcb2124@gmail.com>
2023-04-14 21:01:36 -0400

    wsgi

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 14 20:59:38 2023
+
+@author: dcb21
+"""
+
+import sys
+sys.path.insert(0, '/home/davidbillingsley/') # Replace <username> with your PythonAnywhere username and path/to/parent/folder with the path to the folder containing your cathi.py file.
+
+from cathi import app as application
